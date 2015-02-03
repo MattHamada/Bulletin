491fca2a0c020bfad1e466a767b705e81000cb9b
MattHamada <mattahamada@gmail.com>
2015-02-03 08:45:42 -0700

    updated column name of topic's creator

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,13 +1,19 @@
+require 'time_formatting'
+
 class Topic < ActiveRecord::Base
+  include TimeFormatting
 
   has_many   :posts
-  belongs_to :user   #topic creator
+  belongs_to :creator, class_name: 'User'   #topic creator
   belongs_to :sub_board
 
   validates :title, presence: true, uniqueness: {case_sensitive: false },
-            length: {maximum: 50, minimum: 2 }
+            length: {maximum: 100, minimum: 2 }
 
   before_create :set_post_count
+
+  scope :order_by_time, -> { order("updated_at ASC") }
+
 
   def set_post_count
     self.post_count = 0
