6b2807c1577682d16ecd9dc0c38c67e451977538
MattHamada <mattahamada@gmail.com>
2015-02-11 15:14:19 -0700

    started post creation

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,6 +1,10 @@
 class TopicsController < ApplicationController
   before_action :get_topic
 
+  def new
+    sub_board = SubBoard.find(params[:sub_board])
+    @topic = Topic.new(sub_board_id: sub_board)
+  end
   def show
     @posts = @topic.posts.order_by_time
   end
